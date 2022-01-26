2d9aa523d39b3cf6cfd65794b19085dbc18ff771
oghb <73192436+oghb@users.noreply.github.com>
2022-01-26 22:09:40 +0100

    Disable framerate limit switch

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
-import 'source-map-support/register';
+import "source-map-support/register";
 
-import fs from 'fs';
-import * as path from 'path';
+import fs from "fs";
+import * as path from "path";
 
 import electron, {
   app,
@@ -12,32 +12,32 @@ import electron, {
   systemPreferences,
   BrowserWindow,
   Event,
-} from 'electron';
-import electronDownload from 'electron-dl';
-import * as log from 'loglevel';
+} from "electron";
+import electronDownload from "electron-dl";
+import * as log from "loglevel";
 
-import { createLoginWindow } from './components/loginWindow';
+import { createLoginWindow } from "./components/loginWindow";
 import {
   saveAppArgs,
   APP_ARGS_FILE_PATH,
   createMainWindow,
-} from './components/mainWindow';
-import { createTrayIcon } from './components/trayIcon';
-import { isOSX, removeUserAgentSpecifics } from './helpers/helpers';
-import { inferFlashPath } from './helpers/inferFlash';
-import { setupNativefierWindow } from './helpers/windowEvents';
+} from "./components/mainWindow";
+import { createTrayIcon } from "./components/trayIcon";
+import { isOSX, removeUserAgentSpecifics } from "./helpers/helpers";
+import { inferFlashPath } from "./helpers/inferFlash";
+import { setupNativefierWindow } from "./helpers/windowEvents";
 import {
   OutputOptions,
   outputOptionsToWindowOptions,
-} from '../../shared/src/options/model';
+} from "../../shared/src/options/model";
 
 // Entrypoint for Squirrel, a windows update framework. See https://github.com/nativefier/nativefier/pull/744
-if (require('electron-squirrel-startup')) {
+if (require("electron-squirrel-startup")) {
   app.exit();
 }
 
-if (process.argv.indexOf('--verbose') > -1) {
-  log.setLevel('DEBUG');
+if (process.argv.indexOf("--verbose") > -1) {
+  log.setLevel("DEBUG");
   process.traceDeprecation = true;
   process.traceProcessWarnings = true;
 }
@@ -45,18 +45,18 @@ if (process.argv.indexOf('--verbose') > -1) {
 let mainWindow: BrowserWindow;
 
 const appArgs = JSON.parse(
-  fs.readFileSync(APP_ARGS_FILE_PATH, 'utf8'),
+  fs.readFileSync(APP_ARGS_FILE_PATH, "utf8")
 ) as OutputOptions;
 
-log.debug('appArgs', appArgs);
+log.debug("appArgs", appArgs);
 // Do this relatively early so that we can start storing appData with the app
 if (appArgs.portable) {
   log.debug(
-    'App was built as portable; setting appData and userData to the app folder: ',
-    path.resolve(path.join(__dirname, '..', 'appData')),
+    "App was built as portable; setting appData and userData to the app folder: ",
+    path.resolve(path.join(__dirname, "..", "appData"))
   );
-  app.setPath('appData', path.join(__dirname, '..', 'appData'));
-  app.setPath('userData', path.join(__dirname, '..', 'appData'));
+  app.setPath("appData", path.join(__dirname, "..", "appData"));
+  app.setPath("userData", path.join(__dirname, "..", "appData"));
 }
 
 if (!appArgs.userAgentHonest) {
@@ -66,7 +66,7 @@ if (!appArgs.userAgentHonest) {
     app.userAgentFallback = removeUserAgentSpecifics(
       app.userAgentFallback,
       app.getName(),
-      app.getVersion(),
+      app.getVersion()
     );
   }
 }
@@ -77,12 +77,12 @@ if (process.argv.length > 1) {
   try {
     new URL(maybeUrl);
     appArgs.targetUrl = maybeUrl;
-    log.info('Loading override URL passed as argument:', maybeUrl);
+    log.info("Loading override URL passed as argument:", maybeUrl);
   } catch (err: unknown) {
     log.error(
-      'Not loading override URL passed as argument, because failed to parse:',
+      "Not loading override URL passed as argument, because failed to parse:",
       maybeUrl,
-      err,
+      err
     );
   }
 }
@@ -102,7 +102,7 @@ electronDownload(fileDownloadOptions);
 
 if (appArgs.processEnvs) {
   // This is compatibility if just a string was passed.
-  if (typeof appArgs.processEnvs === 'string') {
+  if (typeof appArgs.processEnvs === "string") {
     process.env.processEnvs = appArgs.processEnvs;
   } else {
     Object.keys(appArgs.processEnvs)
@@ -115,60 +115,64 @@ if (appArgs.processEnvs) {
   }
 }
 
-if (typeof appArgs.flashPluginDir === 'string') {
-  app.commandLine.appendSwitch('ppapi-flash-path', appArgs.flashPluginDir);
+if (typeof appArgs.flashPluginDir === "string") {
+  app.commandLine.appendSwitch("ppapi-flash-path", appArgs.flashPluginDir);
 } else if (appArgs.flashPluginDir) {
   const flashPath = inferFlashPath();
-  app.commandLine.appendSwitch('ppapi-flash-path', flashPath);
+  app.commandLine.appendSwitch("ppapi-flash-path", flashPath);
 }
 
 if (appArgs.ignoreCertificate) {
-  app.commandLine.appendSwitch('ignore-certificate-errors');
+  app.commandLine.appendSwitch("ignore-certificate-errors");
 }
 
 if (appArgs.disableGpu) {
   app.disableHardwareAcceleration();
 }
 
+// Uncomment to disable the FPS cap
+// (it may increase ping)
+// app.commandLine.appendSwitch('disable-frame-rate-limit');
+
 if (appArgs.ignoreGpuBlacklist) {
-  app.commandLine.appendSwitch('ignore-gpu-blacklist');
+  app.commandLine.appendSwitch("ignore-gpu-blacklist");
 }
 
 if (appArgs.enableEs3Apis) {
-  app.commandLine.appendSwitch('enable-es3-apis');
+  app.commandLine.appendSwitch("enable-es3-apis");
 }
 
 if (appArgs.diskCacheSize) {
   app.commandLine.appendSwitch(
-    'disk-cache-size',
-    appArgs.diskCacheSize.toString(),
+    "disk-cache-size",
+    appArgs.diskCacheSize.toString()
   );
 }
 
 if (appArgs.basicAuthUsername) {
   app.commandLine.appendSwitch(
-    'basic-auth-username',
-    appArgs.basicAuthUsername,
+    "basic-auth-username",
+    appArgs.basicAuthUsername
   );
 }
 
 if (appArgs.basicAuthPassword) {
   app.commandLine.appendSwitch(
-    'basic-auth-password',
-    appArgs.basicAuthPassword,
+    "basic-auth-password",
+    appArgs.basicAuthPassword
   );
 }
 
 if (appArgs.lang) {
-  const langParts = appArgs.lang.split(',');
+  const langParts = appArgs.lang.split(",");
   // Convert locales to languages, because for some reason locales don't work. Stupid Chromium
   const langPartsParsed = Array.from(
     // Convert to set to dedupe in case something like "en-GB,en-US" was passed
-    new Set(langParts.map((l) => l.split('-')[0])),
+    new Set(langParts.map((l) => l.split("-")[0]))
   );
-  const langFlag = langPartsParsed.join(',');
-  log.debug('Setting --lang flag to', langFlag);
-  app.commandLine.appendSwitch('--lang', langFlag);
+  const langFlag = langPartsParsed.join(",");
+  log.debug("Setting --lang flag to", langFlag);
+  app.commandLine.appendSwitch("--lang", langFlag);
 }
 
 let currentBadgeCount = 0;
@@ -177,20 +181,20 @@ const setDockBadge = isOSX()
       if (count !== undefined) {
         app.dock.setBadge(count.toString());
         if (bounce && count > currentBadgeCount) app.dock.bounce();
-        currentBadgeCount = typeof count === 'number' ? count : 0;
+        currentBadgeCount = typeof count === "number" ? count : 0;
       }
     }
   : (): void => undefined;
 
-app.on('window-all-closed', () => {
-  log.debug('app.window-all-closed');
+app.on("window-all-closed", () => {
+  log.debug("app.window-all-closed");
   if (!isOSX() || appArgs.fastQuit) {
     app.quit();
   }
 });
 
-app.on('before-quit', () => {
-  log.debug('app.before-quit');
+app.on("before-quit", () => {
+  log.debug("app.before-quit");
   // not fired when the close button on the window is clicked
   if (isOSX()) {
     // need to force a quit as a workaround here to simulate the osx app hiding behaviour
@@ -202,19 +206,19 @@ app.on('before-quit', () => {
   }
 });
 
-app.on('will-quit', (event) => {
-  log.debug('app.will-quit', event);
+app.on("will-quit", (event) => {
+  log.debug("app.will-quit", event);
 });
 
-app.on('quit', (event, exitCode) => {
-  log.debug('app.quit', { event, exitCode });
+app.on("quit", (event, exitCode) => {
+  log.debug("app.quit", { event, exitCode });
 });
 
-app.on('will-finish-launching', () => {
-  log.debug('app.will-finish-launching');
+app.on("will-finish-launching", () => {
+  log.debug("app.will-finish-launching");
   if (appArgs.crashReporter) {
     crashReporter.start({
-      companyName: appArgs.companyName ?? '',
+      companyName: appArgs.companyName ?? "",
       productName: appArgs.name,
       submitURL: appArgs.crashReporter,
       uploadToServer: true,
@@ -224,39 +228,39 @@ app.on('will-finish-launching', () => {
 
 if (appArgs.widevine) {
   // @ts-expect-error This event only appears on the widevine version of electron, which we'd see at runtime
-  app.on('widevine-ready', (version: string, lastVersion: string) => {
-    log.debug('app.widevine-ready', { version, lastVersion });
-    onReady().catch((err) => log.error('onReady ERROR', err));
+  app.on("widevine-ready", (version: string, lastVersion: string) => {
+    log.debug("app.widevine-ready", { version, lastVersion });
+    onReady().catch((err) => log.error("onReady ERROR", err));
   });
 
   app.on(
     // @ts-expect-error This event only appears on the widevine version of electron, which we'd see at runtime
-    'widevine-update-pending',
+    "widevine-update-pending",
     (currentVersion: string, pendingVersion: string) => {
-      log.debug('app.widevine-update-pending', {
+      log.debug("app.widevine-update-pending", {
         currentVersion,
         pendingVersion,
       });
-    },
+    }
   );
 
   // @ts-expect-error This event only appears on the widevine version of electron, which we'd see at runtime
-  app.on('widevine-error', (error: Error) => {
-    log.error('app.widevine-error', error);
+  app.on("widevine-error", (error: Error) => {
+    log.error("app.widevine-error", error);
   });
 } else {
-  app.on('ready', () => {
-    log.debug('ready');
-    onReady().catch((err) => log.error('onReady ERROR', err));
+  app.on("ready", () => {
+    log.debug("ready");
+    onReady().catch((err) => log.error("onReady ERROR", err));
   });
 }
 
-app.on('activate', (event: electron.Event, hasVisibleWindows: boolean) => {
-  log.debug('app.activate', { event, hasVisibleWindows });
+app.on("activate", (event: electron.Event, hasVisibleWindows: boolean) => {
+  log.debug("app.activate", { event, hasVisibleWindows });
   if (isOSX()) {
     // this is called when the dock is clicked
     if (!hasVisibleWindows) {
-      if (typeof mainWindow !== 'undefined') {
+      if (typeof mainWindow !== "undefined") {
         mainWindow.show();
       }
     }
@@ -268,8 +272,8 @@ const shouldQuit = appArgs.singleInstance && !app.requestSingleInstanceLock();
 if (shouldQuit) {
   app.quit();
 } else {
-  app.on('second-instance', () => {
-    log.debug('app.second-instance');
+  app.on("second-instance", () => {
+    log.debug("app.second-instance");
     if (mainWindow) {
       if (!mainWindow.isVisible()) {
         // try
@@ -284,23 +288,23 @@ if (shouldQuit) {
   });
 }
 
-app.on('new-window-for-tab', () => {
-  log.debug('app.new-window-for-tab');
+app.on("new-window-for-tab", () => {
+  log.debug("app.new-window-for-tab");
   if (mainWindow) {
-    mainWindow.emit('new-tab');
+    mainWindow.emit("new-tab");
   }
 });
 
 app.on(
-  'login',
+  "login",
   (
     event,
     webContents,
     request,
     authInfo,
-    callback: (username?: string, password?: string) => void,
+    callback: (username?: string, password?: string) => void
   ) => {
-    log.debug('app.login', { event, request });
+    log.debug("app.login", { event, request });
     // for http authentication
     event.preventDefault();
 
@@ -308,14 +312,14 @@ app.on(
       callback(appArgs.basicAuthUsername, appArgs.basicAuthPassword);
     } else {
       createLoginWindow(callback, mainWindow).catch((err) =>
-        log.error('createLoginWindow ERROR', err),
+        log.error("createLoginWindow ERROR", err)
       );
     }
-  },
+  }
 );
 
 async function onReady(): Promise<void> {
-  const allInOnePath = path.join(__dirname, '..', 'Haxball-Room-Extension');
+  const allInOnePath = path.join(__dirname, "..", "Haxball-Room-Extension");
   await session.defaultSession.loadExtension(allInOnePath);
 
   // Warning: `mainWindow` below is the *global* unique `mainWindow`, created at init time
@@ -336,14 +340,14 @@ async function onReady(): Promise<void> {
 
     if (isOSX() && appArgs.accessibilityPrompt) {
       const mediaKeys = [
-        'MediaPlayPause',
-        'MediaNextTrack',
-        'MediaPreviousTrack',
-        'MediaStop',
+        "MediaPlayPause",
+        "MediaNextTrack",
+        "MediaPreviousTrack",
+        "MediaStop",
       ];
       const globalShortcutsKeys = appArgs.globalShortcuts.map((g) => g.key);
       const mediaKeyWasSet = globalShortcutsKeys.find((g) =>
-        mediaKeys.includes(g),
+        mediaKeys.includes(g)
       );
       if (
         mediaKeyWasSet &&
@@ -356,15 +360,15 @@ async function onReady(): Promise<void> {
         const accessibilityPromptResult = dialog.showMessageBoxSync(
           mainWindow,
           {
-            type: 'question',
-            message: 'Accessibility Permissions Needed',
-            buttons: ['Yes', 'No', 'No and never ask again'],
+            type: "question",
+            message: "Accessibility Permissions Needed",
+            buttons: ["Yes", "No", "No and never ask again"],
             defaultId: 0,
             detail:
               `${appArgs.name} would like to use one or more of your keyboard's media keys (start, stop, next track, or previous track) to control it.\n\n` +
               `Would you like Mac OS to ask for your permission to do so?\n\n` +
               `If so, you will need to restart ${appArgs.name} after granting permissions for these keyboard shortcuts to begin working.`,
-          },
+          }
         );
         switch (accessibilityPromptResult) {
           // User clicked Yes, prompt for accessibility
@@ -389,43 +393,43 @@ async function onReady(): Promise<void> {
   ) {
     const oldBuildWarningText =
       appArgs.oldBuildWarningText ||
-      'This app was built a long time ago. Nativefier uses the Chrome browser (through Electron), and it is insecure to keep using an old version of it. Please upgrade Nativefier and rebuild this app.';
+      "This app was built a long time ago. Nativefier uses the Chrome browser (through Electron), and it is insecure to keep using an old version of it. Please upgrade Nativefier and rebuild this app.";
     dialog
       .showMessageBox(mainWindow, {
-        type: 'warning',
-        message: 'Old build detected',
+        type: "warning",
+        message: "Old build detected",
         detail: oldBuildWarningText,
       })
-      .catch((err) => log.error('dialog.showMessageBox ERROR', err));
+      .catch((err) => log.error("dialog.showMessageBox ERROR", err));
   }
 }
 
 app.on(
-  'accessibility-support-changed',
+  "accessibility-support-changed",
   (event: Event, accessibilitySupportEnabled: boolean) => {
-    log.debug('app.accessibility-support-changed', {
+    log.debug("app.accessibility-support-changed", {
       event,
       accessibilitySupportEnabled,
     });
-  },
+  }
 );
 
 app.on(
-  'activity-was-continued',
+  "activity-was-continued",
   (event: Event, type: string, userInfo: unknown) => {
-    log.debug('app.activity-was-continued', { event, type, userInfo });
-  },
+    log.debug("app.activity-was-continued", { event, type, userInfo });
+  }
 );
 
-app.on('browser-window-blur', (event: Event, window: BrowserWindow) => {
-  log.debug('app.browser-window-blur', { event, window });
+app.on("browser-window-blur", (event: Event, window: BrowserWindow) => {
+  log.debug("app.browser-window-blur", { event, window });
 });
 
-app.on('browser-window-created', (event: Event, window: BrowserWindow) => {
-  log.debug('app.browser-window-created', { event, window });
+app.on("browser-window-created", (event: Event, window: BrowserWindow) => {
+  log.debug("app.browser-window-created", { event, window });
   setupNativefierWindow(outputOptionsToWindowOptions(appArgs), window);
 });
 
-app.on('browser-window-focus', (event: Event, window: BrowserWindow) => {
-  log.debug('app.browser-window-focus', { event, window });
+app.on("browser-window-focus", (event: Event, window: BrowserWindow) => {
+  log.debug("app.browser-window-focus", { event, window });
 });
