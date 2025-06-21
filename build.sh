3a510f608247b6ad285e9c0516be6ba6eb699e3b
oghb <73192436+oghb@users.noreply.github.com>
2025-06-21 13:55:37 +0200

    Finalize build script

--- build.sh
+++ build.sh
@@ -7,14 +7,14 @@ fi
 cd ..
 
 electron-builder --mac --universal
-# electron-builder --win --arm64 --x64 --ia32
-# electron-builder --linux AppImage --x64
-# electron-builder --linux deb --x64
+electron-builder --win --arm64 --x64 --ia32
+electron-builder --linux AppImage --x64
+electron-builder --linux deb --x64
 
-# cd dist
+cd dist
 
-# # # extract the version part from the filename using a regex
-# VERSION=$(echo $(ls | grep 'HaxBall Client.*\.exe') | sed -E 's/HaxBall Client-([0-9]+\.[0-9]+\.[0-9]+)-.*/\1/')
+# # extract the version part from the filename using a regex
+VERSION=$(echo $(ls | grep 'HaxBall Client.*\.exe') | sed -E 's/HaxBall Client-([0-9]+\.[0-9]+\.[0-9]+)-.*/\1/')
 
-# zip -r "HaxBall Client-${VERSION}-win-portable.zip" "HaxBall Client-${VERSION}.exe"
-# zip -r "HaxBall Client-${VERSION}-linux.zip" *.deb *.AppImage
+zip -r "HaxBall Client-${VERSION}-win-portable.zip" "HaxBall Client-${VERSION}.exe"
+zip -r "HaxBall Client-${VERSION}-linux.zip" *.deb *.AppImage
