e3af357570f94681ae01cf5dd743376820b71bfb
oghb <73192436+oghb@users.noreply.github.com>
2025-06-29 11:39:08 +0200

    Removed all-in-one repo cloning on build

--- build.sh
+++ build.sh
@@ -1,11 +1,3 @@
-# Clone the Haxball-Room-Extension if not present
-cd inject
-if [ ! -d "Haxball-Room-Extension" ]; then
-	git clone https://github.com/xenonsb/Haxball-Room-Extension.git
-fi
-
-cd ..
-
 electron-builder --mac --universal
 electron-builder --win --arm64 --x64 --ia32
 electron-builder --linux AppImage --x64
@@ -13,7 +5,7 @@ electron-builder --linux deb --x64
 
 cd dist
 
-# # extract the version part from the filename using a regex
+# extract the version part from the filename using a regex
 VERSION=$(echo $(ls | grep 'HaxBall Client.*\.exe') | sed -E 's/HaxBall Client-([0-9]+\.[0-9]+\.[0-9]+)-.*/\1/')
 
 zip -r "HaxBall Client-${VERSION}-win-portable.zip" "HaxBall Client-${VERSION}.exe"
