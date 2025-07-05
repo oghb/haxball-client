7609fcd66322b3022ef278fd1074fa88d20e8b49
oghb <73192436+oghb@users.noreply.github.com>
2025-07-05 12:09:26 +0200

    Deleted all command bar code

--- inject/src/ui/setupUIElements.ts
+++ inject/src/ui/setupUIElements.ts
@@ -1,10 +1,8 @@
 import { removeAds } from "./removeAds";
-import { setupCommandBar } from "./setupCommandBar";
 import { addAddressBarToHeader, setupCustomHeader } from "./setupCustomHeader";
 
 export const setupUIElements = async (): Promise<void> => {
 	await removeAds();
 	await setupCustomHeader();
 	addAddressBarToHeader();
-	await setupCommandBar();
 }
